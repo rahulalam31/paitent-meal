fddd34c7a63ce8686681795ff78490202fb6df63
rahulalam31 <gaberialcramern31@outlook.com>
2024-05-22 11:07:00 +0530

    model and route update

--- app/Models/paitent_meal_planning.php
+++ app/Models/paitent_meal_planning.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class paitent_meal_planning extends Model
 {
     use HasFactory;
+
+    public function patient()
+    {
+        return $this->belongsTo(patient::class, 'paitent_id', 'id');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ use App\Http\Controllers\PaitentMealPlanningController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('patient-meal-planning/stats', [App\Http\Controllers\PaitentMealPlanningController::class, 'getMonthlyStats']);
+Route::get('patient-meal-planning/stats', [App\Http\Controllers\PaitentMealPlanningController::class, 'getMonthlyStats'])->name('api.patient-meal-planning.stats');
